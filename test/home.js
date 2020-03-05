c5abae8a7d88e4cf9f998c0579d2f8c41ddb6ac1
tux7P <lkwa1687@tuta.io>
2020-03-05 01:46:38 +0100

    Add css and xpath selector example for practice

--- test/home.js
+++ test/home.js
@@ -5,6 +5,15 @@ describe('The Internet Home Page', () => {
         browser.url('https://the-internet.herokuapp.com/')
         const title = browser.getTitle()
         assert.strictEqual(title, 'The Internet')
+
+        let headingText = $(".heading").getText()
+        console.log(headingText)
+
+        let footerText = $("#page-footer").getText()
+        console.log(footerText)
+
+        let footerXpath = $("//*[@id='page-footer']").getText()
+        console.log(footerXpath)
     })
     it('Should have right page header - h1', () => {})
     it('Should have right sub header - h2', () => {})
