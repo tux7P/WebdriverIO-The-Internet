970c8a6673d657ecaaf731f3888ccae44108b17e
tux7P <lkwa1687@tuta.io>
2020-03-07 22:43:28 +0100

    Organize test cases

--- test/home.js
+++ test/home.js
@@ -5,18 +5,22 @@ describe('The Internet Home Page', () => {
         browser.url('https://the-internet.herokuapp.com/')
         const title = browser.getTitle()
         assert.strictEqual(title, 'The Internet')
-
+    })
+    it('Should have right page header - h1', () => {
         let headingText = $(".heading").getText()
         console.log(headingText)
-
+    })
+    it('Should have right sub header - h2', () => {
+        
+    })
+    it('Should have footer', () => {
         let footerText = $("#page-footer").getText()
         console.log(footerText)
 
         let footerXpath = $("//*[@id='page-footer']").getText()
         console.log(footerXpath)
     })
-    it('Should have right page header - h1', () => {})
-    it('Should have right sub header - h2', () => {})
-    it('Should have footer', () => {})
-    it('Should have github fork ribbon', () => {})
+    it('Should have github fork ribbon', () => {
+
+    })
 })
