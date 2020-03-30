adfd0c71bc1b261272b5fa3ea7bcbd48cf5c2cff
tux7P <lkwa1687@tuta.io>
2020-03-30 22:23:59 +0100

    Fix base url

--- test/elementActions.test.js
+++ test/elementActions.test.js
@@ -1,2 +1,9 @@
 const homePage = require('../pages/home.page')
 
+describe ('Test element action', function() {
+    it('should click element', () => {
+        browser.url('/')
+        homePage.clickOnLink()
+        expect(browser.getUrl()).equals('https://the-internet.herokuapp.com/abtest')
+    })
+})
